47880a9e20a400aee14dd827e5defe9e4f516179
Kiley003 <KileyFeist@gmail.com>
2025-11-14 10:03:12 -0500

    Create Module3_Programming Assignment1.java

---
+++ Module3_Programming Assignment1.java
@@ -0,0 +1,15 @@
+public class Module3_Programming Assignment1
+  /** Exercise 13_9 */
+
+  public static void main(String[] args) {
+    Circle circle1 = new Circle(10.00);
+     Circle c2 = new Circle(56.22);
+        Circle c3 = new Circle(69.11);
+        Circle c4 = new Circle(45.00);
+
+        System.out.println("c1.equals(c2): " + c1.equals(c2));
+        System.out.println("c1.equals(c4)" + c1.equals(c4));
+        System.out.println("c2.compareTo(c3)" + c2.compareTo(c3));
+    }
+}
+
